a22e123038f4bb6b0c4b6e84d86313319a8cb66b
James Stine <james.stine@okstate.edu>
2022-08-03 12:10:30 -0500

    minor tweaks

--- synth/clean_me.csh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -f *.cmd*
-rm -f *.log*
-rm -f synth.log
-rm -f reports/*
-
-
-
--- synth/fv/mult_seq/read_libs.tcl
+++ synth/fv/mult_seq/read_libs.tcl
@@ -1,3 +1,3 @@
 add_search_path ../sky130_osu_sc_t18/18T_ms/lib -library -both
-read_library -liberty -both /home/rerra/sky130_cds/synth/../sky130_osu_sc_t18/18T_ms/lib/sky130_osu_sc_18T_ms_TT_1P8_25C.ccs.lib
+read_library -liberty -both /home/jstine/Desktop/sky130_cds/synth/../sky130_osu_sc_t18/18T_ms/lib/sky130_osu_sc_18T_ms_TT_1P8_25C.ccs.lib
 
